e4d3ff00bb39340d4e196c7dddf0bf71be66f1b4
Jet Hughes <hugje043@student.otago.ac.nz>
2022-06-12 11:42:46 +1200

    Added Home UI

--- generator/src/screens/HomeScreen.js
+++ generator/src/screens/HomeScreen.js
@@ -1,12 +1,50 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import { StyleSheet, Text, View, Alert } from 'react-native';
+import Button from '../components/Button'
 
 function HomeScreen() {
     return(   
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Home Screen</Text>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <View style={styles.trickContainer}>
+                <Text style={styles.trick}>BACK FLIP</Text>
+            </View>
+            <View style={styles.mainButtons}>
+                <Button onPress={myFunction} title="RAILS"/>
+                <Button onPress={myFunction} title="JUMPS"/>
+                <Button onPress={myFunction} title="PIPE"/>
+            </View>
+            <View style={styles.optionsButtonContainer}>
+                <Button onPress={myFunction} title="OPTIONS"/>
+            </View>
         </View>
     );
 }
 
+function myFunction(){
+    Alert.alert('Right button pressed')
+}
+
 export default HomeScreen;
+
+const styles = StyleSheet.create({
+    trick: {
+        fontSize: 70,
+        fontWeight: 'bold',
+        textAlign: "center"
+    },
+    trickContainer: {
+        width: 300,
+        height: 50,
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    mainButtons: {
+        marginTop:10,
+        marginBottom:10
+    },
+    optionsButtonContainer: {
+        marginTop: 10,
+        marginBottom: 30
+    }
+})
